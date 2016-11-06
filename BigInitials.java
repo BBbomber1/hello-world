b201280bc75e63b9a8918af14507c51621844450
BBbomber1 <nguyen_david1949@yahoo.com>
2016-11-06 09:34:43 -0500

    Add files via upload

---
+++ BigInitials.java
@@ -0,0 +1,20 @@
+/*
+Challenge Activity 3
+Write a java program to print your initials in "large letters"
+For example:
+M     M  FFFFFF
+M M M M  F
+M  M  M  FFFF
+M     M  F
+M     M  F
+ */
+
+public class BigInitials {
+	public static void main(String[]args) {
+		System.out.println("DDDDDDDD  N      N");
+		System.out.println("D     DDD N N    N");
+		System.out.println("D     DDD N  N   N");
+		System.out.println("D     DD  N   N  N");
+		System.out.println("DDDDDDD   N    N N");
+	}
+}
